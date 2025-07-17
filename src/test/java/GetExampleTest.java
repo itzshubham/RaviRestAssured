9ac7e48cdbf04b85fd596597356481af729a498d
Shubham Mathur <Shubham.mathur1@builder.ai>
2025-07-17 20:01:31 +0530

    initial api code

---
+++ src/test/java/GetExampleTest.java
@@ -0,0 +1,22 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+
+public class GetExampleTest {
+	
+	@Test
+	public void getUsers() {
+		Response res=given().baseUri("https://reqres.in/").when().get("api/users?page=2");
+		Assert.assertTrue(res.statusCode()==200);
+		JsonPath path = res.jsonPath();
+		String s=path.get("support.url").toString();
+		System.out.println("anser="+s);
+		Assert.assertTrue(s.contains("contentcaddy.io"));
+		
+	}
+
+}
